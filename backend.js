c52cc9b857f994278acc29c20406e7544be44853
praneeth2025 <vamsipraneeth2004@gmail.com>
2025-05-08 06:35:37 +0530

    Set up backend with Express

--- backend.js
+++ backend.js
@@ -972,7 +972,7 @@ app.get("/pending-_tests/:patient_id", async (req, res) => {
     return res.status(500).json({ message: 'Unexpected error occurred.' });
   }
 });
-
-  app.listen(3000, () => {
-    console.log('Server running on port 3000');
-  });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
